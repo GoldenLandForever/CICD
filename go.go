57e074a6065136b0e1be0a870b270b17b0103c80
zkx <1339366275@qq.com>
2025-12-30 17:37:15 +0800

    go mod

--- go.go
+++ go.go
@@ -7,7 +7,8 @@ import (
 
 func main() {
 	var wg sync.WaitGroup
-	ch := make(chan bool) // 用于同步信号
+	chodd := make(chan bool)  // 用于同步信号
+	cheven := make(chan bool) // 用于同步信号
 
 	wg.Add(2)
 
@@ -15,8 +16,9 @@ func main() {
 	go func() {
 		defer wg.Done()
 		for i := 1; i <= 99; i += 2 {
+			<-cheven // 等待偶数信号
 			fmt.Println("Odd:", i)
-			ch <- true // 发送信号给偶数
+			chodd <- true // 发送信号给偶数
 		}
 	}()
 
@@ -24,16 +26,18 @@ func main() {
 	go func() {
 		defer wg.Done()
 		for i := 2; i <= 100; i += 2 {
-			<-ch // 等待奇数信号
+			<-chodd // 等待奇数信号
 			fmt.Println("Even:", i)
 			if i < 100 {
-				ch <- true // 发送信号给下一个奇数
+				cheven <- true // 发送信号给下一个奇数
 			}
 		}
 	}()
 
-	// 开始：奇数先打印，无需初始信号
+	// 启动打印，从奇数开始
+	cheven <- true
 	wg.Wait()
-	close(ch)
+	close(chodd)
+	close(cheven)
 	fmt.Println("Done printing 1 to 100 alternately.")
 }
